20d4847bc1da0b079c775e0e85bbc276d32296f5
fedor <btm.007@mail.ru>
2021-05-19 21:48:23 +0300

    Added some configs for faster setup

--- ima/ima/settings.py
+++ ima/ima/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,11 @@ SECRET_KEY = '$!m%#olg1j3ee!2o5%^(q#w)q_3l(uhwtn%s8sl%bf1ed--ls3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.1.7',
+    '[200:e2e6:cb4e:7d5e:dac7:42fa:14ea:c192]',
+    'yumi.yumi.nari',
+]
 
 
 # Application definition
@@ -117,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
